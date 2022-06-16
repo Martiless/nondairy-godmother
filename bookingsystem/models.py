a79ec06d0ddf90022b4f7c11603d1c198c939621
Martina Martin <marti.law@gmail.com>
2022-06-16 09:06:30 +0000

    Add booking and table model to models.py file

--- bookingsystem/models.py
+++ bookingsystem/models.py
@@ -2,11 +2,25 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Customer(models.Model):
-
-class Table(models.Model):
+# class Customer(models.Model):
 
 class Booking(models.Model):
+    name = models.CharField(max_length=50)
+    email_address = models.EmailField()
+    phone = models.IntegerField()
+    pax = models.IntegerField()
+    date = models.DateField()
+    time = models.TimeField()
+    occasion = models.TextField(max_length=500)
+
+    def __str__(self):
+        return self.name
 
 
-    
+class Table(models.Model):
+    number_of_seats = models.IntegerField()
+    max_pax = models.IntegerField()
+    min_pax = models.IntegerField()
+
+    def __str__(self):
+        return self.number_of_seats

0493c62edfecd48f7b1bad2ce93714441923a374
Martina Martin <marti.law@gmail.com>
2022-06-20 18:40:23 +0000

    First edit to views.py & urls.py

--- bookingsystem/urls.py
+++ bookingsystem/urls.py
@@ -5,8 +5,8 @@ app_name = 'bookingsystem'
 
 
 urlpatterns = [
-    path('', views.Home, name='home'),
-    path('', views.BookingForm, name='booking'),
-    path('', views.Menus, name='menus'),
-    path('', views.Login, name='login'),
+    path('', views.Home.as_view(), name='home'),
+    path('', views.BookingForm.as_view(), name='booking'),
+    path('', views.Menus.as_view(), name='menus'),
+    path('', views.Login.as_view(), name='login'),
 ]
--- bookingsystem/views.py
+++ bookingsystem/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from django.views import generic
 from .models import Booking
 
+
 class Home(generic.DetailView):
     template_name = 'index.html'
+
+    def get_queryset(self):
+          return HttpResponseRedirect()
+
 
 class BookingForm(generic.FormView):
     form = Booking
     template_name = 'bookings.html'
 
+
 class Menus(generic.DetailView):
     template_name = 'menus.html'
+
 
 class Login(generic.DetailView):
     template_name = 'login.html'

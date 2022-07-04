8a0396d9b4672511b34123603a17927b675e65b6
Martina Martin <marti.law@gmail.com>
2022-07-04 07:59:43 +0000

    Add date and time pickers to booking form

--- bookingsystem/forms.py
+++ bookingsystem/forms.py
@@ -1,9 +1,21 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Booking
+
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+
+class TimeInput(forms.TimeInput):
+    input_type = 'time'
 
 
 class OnlineForm(ModelForm):
     class Meta:
         model = Booking
         fields = '__all__'
-
+        widgets = {
+            'date': DateInput(),
+            'time': TimeInput()
+        }

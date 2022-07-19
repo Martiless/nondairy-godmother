1073f32b548de6daa42b8efd2499a28a73e825a0
Martina Martin <marti.law@gmail.com>
2022-07-19 08:58:52 +0000

    Add comments to forms.py file

--- bookingsystem/forms.py
+++ bookingsystem/forms.py
@@ -4,17 +4,27 @@ from .models import Booking
 
 
 class DateInput(forms.DateInput):
+    """
+    This class provides a widget for use in the
+    booking form. It provides a calendar for users
+    to pick the booking date from
+    """
     input_type = 'date'
 
 
-class TimeInput(forms.TimeInput):
-    input_type = 'time'
-
-
 class OnlineForm(ModelForm):
+    """
+    This form is connected with the view
+    in order to provide users with the neccessary
+    fields for making a booking
+    """
     class Meta:
+        """Defines which model to pull the
+        fields from"""
         model = Booking
+        # Tell the form to use all the fields provided
         fields = '__all__'
+        # Except fot the user field
         exclude = ('user', )
         widgets = {
             'date': DateInput()

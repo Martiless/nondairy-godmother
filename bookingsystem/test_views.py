20a9159f99cd593e2880667ada46b341b669bde8
Martina Martin <marti.law@gmail.com>
2022-08-08 09:49:56 +0000

    Set up test_views.py

--- bookingsystem/test_views.py
+++ bookingsystem/test_views.py
@@ -5,9 +5,12 @@ from bookingsystem.models import Booking, SignUp
 
 class TestViews(TestCase):
 
+    def setUp(self):
+        self.client = Client()
+        self.home_url = reverse('Home')
+
     def test_home_view_GET(self):
-        client = Client()
-        response = client.get(reverse('Home'))
+        response = self.client.get(self.home_url)
 
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'index.html')
+        self.assertTemplateUsed(response, 'bookingsystem/')

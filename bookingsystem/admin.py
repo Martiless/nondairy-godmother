2ee079e74c7413f4a14507b5c885bf8ff13dc461
Martina Martin <marti.law@gmail.com>
2022-08-08 07:36:33 +0000

    Add Signup to admin.py

--- bookingsystem/admin.py
+++ bookingsystem/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from .models import Booking
+from .models import Booking, SignUp
 
 
 @admin.register(Booking)
 class BookingAdmin(admin.ModelAdmin):
     model = Booking
     list_display = ('name', 'email_address')
+
+
+@admin.register(Signup)
+class SignUpAdmin(admin.ModelAdmin):
+    model = SignUp
+    list_display = ('__all__')

999894062fcfb600f774b09e50138489f4219ee3
Martina Martin <marti.law@gmail.com>
2022-08-09 08:38:09 +0000

    Edit settings.py to allow testing

--- nondairygodmother/settings.py
+++ nondairygodmother/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import sys
 import os
 # Code taken from Code Institutes walkthrough project
 import dj_database_url
@@ -102,16 +103,18 @@ WSGI_APPLICATION = 'nondairygodmother.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#          'ENGINE': 'django.db.backends.sqlite3',
-#          'NAME': BASE_DIR / 'db.sqlite3',
-#      }
-#  }
+if 'test' in sys.argv:
+    DATABASES = {
+     'default': {
+          'ENGINE': 'django.db.backends.sqlite3',
+          'NAME': BASE_DIR / 'db.sqlite3',
+      }
+    }
 
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+        }
 
 
 # Password validation
